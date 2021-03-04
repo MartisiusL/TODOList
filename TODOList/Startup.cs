e443ecfea0d28748280e9fb7776488d0c79ef47e
MartisiusL <31843212+MartisiusL@users.noreply.github.com>
2021-03-04 14:46:45 +0200

    Add Role Based Authorization

--- TODOList/Startup.cs
+++ TODOList/Startup.cs
@@ -1,15 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using TODOList.Helpers;
+using TODOList.Services;
 
 namespace TODOList
     {
@@ -25,7 +23,36 @@ namespace TODOList
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices (IServiceCollection services)
             {
+            services.AddCors ();
             services.AddControllers ();
+
+            // configure strongly typed settings objects
+            var appSettingsSection = Configuration.GetSection ("AppSettings");
+            services.Configure<AppSettings> (appSettingsSection);
+
+            // configure jwt authentication
+            var appSettings = appSettingsSection.Get<AppSettings> ();
+            var key = Encoding.ASCII.GetBytes (appSettings.Secret);
+            services.AddAuthentication (x =>
+                {
+                    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer (x =>
+                {
+                    x.RequireHttpsMetadata = false;
+                    x.SaveToken = true;
+                    x.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey (key),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
+
+            // configure DI for application services
+            services.AddScoped<IUserService, UserService> ();
             }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,12 +67,17 @@ namespace TODOList
 
             app.UseRouting ();
 
+            app.UseCors (x => x
+                .AllowAnyOrigin ()
+                .AllowAnyMethod ()
+                .AllowAnyHeader ());
+
+            app.UseAuthentication ();
             app.UseAuthorization ();
 
-            app.UseEndpoints (endpoints =>
-             {
-                 endpoints.MapControllers ();
-             });
+            app.UseEndpoints (endpoints => {
+                endpoints.MapControllers ();
+            });
             }
         }
     }
